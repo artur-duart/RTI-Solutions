832471c38882500a9b8179a111cf5b2bfbc618f8
Artur Duarte <arturduartemoraes@gmail.com>
2021-08-12 19:21:50 -0300

    PHP do Fulbank concluído

--- RTI_Solutions/Fulbank/calcular.php
+++ RTI_Solutions/Fulbank/calcular.php
@@ -1,13 +1,18 @@
 <?php
 
-if (isset($_POST["cidadeOrigem"]) && isset($_POST["cidadeDestino"]) && isset($_POST["distancia"]) && isset($_POST["pedagios"])){
+if (isset($_POST["nome"]) && isset($_POST["salario"]) && isset($_POST["genero"]) && isset($_POST["cargos"])){
 
-    $cidadeOrigem = $_POST["cidadeOrigem"];
-    $cidadeDestino = $_POST["cidadeDestino"];
-    $distancia = $_POST["distancia"];
-    $pedagios = $_POST["pedagios"];
-    $totalViagemBruto = ($pedagios * 9.40) +  ($distancia * 6);
-    $totalViagem = number_format((float)$totalViagemBruto, 2, ',' , '');  
+    $nome = $_POST["nome"];
+    $salario = $_POST["salario"];
+    $genero = $_POST["genero"];
+    $cargos = $_POST["cargos"];
+
+    if ($salario > 5000) {
+        $novoSalario = ((10 / 100) * $salario) + $salario;
+    } else {
+        $novoSalario = ((20 / 100) * $salario) + $salario;
+    }
+
 } else {
     echo "ERRO AO ENVIAR FORMULÁRIO!";
 }
@@ -29,35 +34,35 @@ if (isset($_POST["cidadeOrigem"]) && isset($_POST["cidadeDestino"]) && isset($_P
     <form method="POST" action="calcular.php">
         <p>Aumento Salarial</p>
 
-            <p>Aumento Salarial</p>
+        <label for="nome">
+            Nome
+            <input class="texto" type="text" id="nome" name="nome" value="<?= $nome ?>" readonly>
+        </label>
 
-            <label for="nome">
-                Nome
-                <input class="texto" type="text" id="nome" name="nome" required>
-            </label>
+        <label for="salario">
+            Antigo Salário (em R$)
+            <input class="texto" type="number" id="salario" name="salario" value="<?= $salario ?>" readonly>
+        </label>
 
-            <label for="salario">
-                Salário (em R$)
-                <input class="texto" type="text" id="salario" name="salario" required>
-            </label>
+        <div class="generos">
+            <label>Gênero:</label>
+            <label for="masculino">
+                <input type="radio"  name="genero" checked value="<?= $genero ?>"><?= $genero ?>
+            </label> 
 
-            <div class="generos">
-                <label>Gênero:</label>
-                <label for="masculino">
-                    <input type="radio" id="masculino" name="genero" value="masculino">Masculino
-                </label>
+            <br>
+            <br>
+            <br>
 
-                <label for="feminino">
-                    <input type="radio" id="feminino" name="genero" value="feminino">Feminino
-                </label>
-
-                <label for="outros">
-                    <input type="radio" id="outros" name="genero" value="outros">Outros
-                </label>
-            </div>
+        <label id="cargosLabel" for="cargos">
+            Cargos
+            <select id="cargos" name="cargos" disabled>
+                <option id="selecione" selected value=""><?= $cargos ?></option>
+            </select>
+        </label>
 
 
-        <textarea id="total" cols="22" rows="4" readonly>A viagem de <?= $cidadeOrigem ?> para <?= $cidadeDestino ?> irá custar o valor total de R$<?= $totalViagem ?></textarea>
+        <textarea id="total" cols="22" rows="4" readonly>Parabéns <?= $nome ?>! Você passará a receber R$<?= $novoSalario ?> no cargo de <?= $cargos ?>!</textarea>
 
     </form>
 
